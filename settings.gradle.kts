0d9127f4f75a7c4429a6bb288356d5142f686381
Robert Zetzsche <zetzsche@movisens.com>
2023-01-07 23:24:47 +0100

    feat: added kodein and sqldelight

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -11,6 +11,8 @@ dependencyResolutionManagement {
             version("ktor", "2.2.2")
             version("coroutines", "1.6.4")
             version("jb-compose", extra.get("composeVersion").toString())
+            version("kodein", "7.16.0")
+            version("sqldelight", extra.get("sqldelight").toString())
         }
     }
 }
@@ -26,6 +28,7 @@ pluginManagement {
     extra.set("kotlinVersion", "1.7.20")
     extra.set("agpVersion", "7.3.1")
     extra.set("composeVersion", "1.3.0-rc01")
+    extra.set("sqldelight", "1.5.4")
 
     plugins {
         kotlin("jvm").version(extra.get("kotlinVersion").toString())
@@ -36,10 +39,9 @@ pluginManagement {
         id("com.android.application").version(extra.get("agpVersion").toString())
         id("com.android.library").version(extra.get("agpVersion").toString())
         id("org.jetbrains.compose").version(extra.get("composeVersion").toString())
+        id("com.squareup.sqldelight").version(extra.get("sqldelight").toString())
     }
 }
-
-
 
 rootProject.name = "compose_mp_ios"
 
