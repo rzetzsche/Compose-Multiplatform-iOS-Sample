48c63d8ebc1355c3cb6995a29808c6c4d125b4d0
Robert Zetzsche <zetzsche@movisens.com>
2023-01-07 22:33:58 +0100

    fix: desktop build

--- desktopApp/src/jvmMain/kotlin/com/plauzeware/compose_mp_ios/Main.kt
+++ desktopApp/src/jvmMain/kotlin/com/plauzeware/compose_mp_ios/Main.kt
@@ -1,10 +1,13 @@
 package com.plauzeware.compose_mp_ios
 
 import androidx.compose.material.MaterialTheme
+import androidx.compose.ui.window.application
 import moe.tlaster.precompose.PreComposeWindow
 
-fun main() = PreComposeWindow(onCloseRequest = {}) {
-    MaterialTheme {
-        MainDesktopView()
+fun main() = application {
+    PreComposeWindow(onCloseRequest = { exitApplication() }) {
+        MaterialTheme {
+            MainDesktopView()
+        }
     }
-}
+} 
