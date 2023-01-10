7ecde150ab2cf1995e53ced95b36344700a75ebf
Robert Zetzsche <zetzsche@movisens.com>
2023-01-10 21:58:01 +0100

    fix: renamed controller to mainviewcontroller

--- iosApp/iosApp/iosApp.swift
+++ iosApp/iosApp/iosApp.swift
@@ -7,7 +7,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         window = UIWindow(frame: UIScreen.main.bounds)
-        let mainViewController = Main_iosKt.MainViewController()
+        let mainViewController = MainViewControllerKt.MainViewController()
         window?.rootViewController = mainViewController
         window?.makeKeyAndVisible()
         return true
